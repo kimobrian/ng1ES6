a50e839ee79ca5ba137ac502ffe4b048096071ff
kimobrian <brian.kimokoti@andela.com>
2017-08-25 02:28:33 +0300

    feat: Include and illustrate DI using ui-router

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
-
+import MathModule from './math';
+import uiRouter from 'angular-ui-router';
 import '../style/app.scss';
 
 class AppDirective {
@@ -12,19 +13,50 @@ class AppDirective {
 }
 
 class AppCtrl {
-  constructor() {
-
+  constructor(mathSvc) {
+      'ngInject'
+      this.mathSvc = mathSvc;
   }
 
   $onInit() {
-      this.url = 'https://github.com/kimobrian/ng1ES6';
+      this.a = 'John';
+      this.b = 'Doe';
+      this.url = `https://github.com/kimobrian/ng1ES6`;
+      this.fullName = this.a + ' '+ this.b;
   }
+
+  $doCheck() {
+      var previousValue;
+      var currentValue = this.a + ' '+ this.b;
+      if (previousValue !== currentValue) {
+        this.fullName = this.a + ' '+ this.b;
+        previousValue = currentValue;
+      }
+  }
+}
+
+let testComponent = {
+    template: '<p>This is a test component</p>'
 }
 
 const MODULE_NAME = 'app';
 
-let app = angular.module(MODULE_NAME, [])
+let app = angular.module(MODULE_NAME, [MathModule.name, uiRouter])
   .directive('app', () => new AppDirective())
-  .controller('AppCtrl', AppCtrl);
+  .component('test', testComponent)
+  .controller('AppCtrl', AppCtrl)
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+      $stateProvider
+             .state('home', {
+                url: '/home',
+                component: 'mathComponent'
+            })
+            .state('test', {
+                url:'/test',
+                component: 'test'
+            })
+      $urlRouterProvider.otherwise('/home')
+  })
 
 export default app;
